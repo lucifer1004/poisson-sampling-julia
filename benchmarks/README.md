e28f25b77de292a7c5f91796d769cbb926079413
Gabriel Wu <wuzihua@pku.edu.cn>
2022-09-05 18:30:28 +0800

    upgrade benchmarks

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -2,30 +2,21 @@
 
 ## Introduction
 In this benchmark, we compare three Poisson Disk Sampling implementations among
-Taichi, Numpy, and Numba.
+Taichi, Numba, and Julia.
 The algorithm is suitable for single-threaded performance comparisons.
 <p align="center">
 <img src="fig/demo.gif" height="370px" />
 </p>
 
 ## Evaluation
-We conduct performance evaluation on the following device.
-
-|Device| Nvidia RTX 3080 (10GB)|
-|-----|-----------------------|
-|FP32 performance| 29700 GFLOPS|
-|Memory bandwidth| 760 GB/s|
-|L2 cache capacity| 5 MB|
-|Driver version| 470.57.02|
-|CUDA version| 11.4|
 
 Performance is measured in milliseconds (ms), we run over different
 number of samples.
 The reported times are measured using a 400 x 400 grid. 
 The employed Taichi version 
-is [1.0.0](https://github.com/taichi-dev/taichi/releases/tag/v1.0.0), 
-the Numpy version is 1.21.5, the Numba version is 1.55.1, and the 
-python version is 3.9.
+is [1.1.2](https://github.com/taichi-dev/taichi/releases/tag/v1.1.2), 
+the Numpy version is 1.23.2, the Numba version is 0.56.2, the 
+Python version is 3.9.12, and the Julia version is 1.8.0.
 
 <p align="center">
 <img src="fig/bench.png" width="600">
@@ -38,6 +29,7 @@ python version is 3.9.
 python3 -m pip install numpy numba
 python3 -m pip install taichi
 python3 -m pip install matplotlib
+python3 -m pip install juliacall
 ```
 * Run the benchmark and draw the plots
 ```shell
--- benchmarks/plot_benchmark.py
+++ benchmarks/plot_benchmark.py
@@ -6,14 +6,14 @@ from src.taichi.benchmark import benchmark as benchmark_taichi
 from src.julia.benchmark import benchmark as benchmark_julia
 from src.numba.benchmark import benchmark as benchmark_numba
 
-numba_sample_results = {'numba_cpu': [{'desired_samples': 1000, 'time_ms': 8.600405813194811}, {'desired_samples': 5000, 'time_ms': 53.80218180362135}, {'desired_samples': 10000,
-                                                                                                                                                         'time_ms': 115.28392401523888}, {'desired_samples': 50000, 'time_ms': 637.3968123923987}, {'desired_samples': 100000, 'time_ms': 689.3348993966356}]}
+numba_sample_results = {'numba_cpu': [{'desired_samples': 1000, 'time_ms': 8.729429193772376}, {'desired_samples': 5000, 'time_ms': 54.30453319568187}, {
+    'desired_samples': 10000, 'time_ms': 114.32691961526871}, {'desired_samples': 50000, 'time_ms': 638.4390128077939}, {'desired_samples': 100000, 'time_ms': 682.2548018069938}]}
 
-taichi_sample_results = {'taichi_cpu': [{'desired_samples': 1000, 'time_ms': 16.961263469420373}, {'desired_samples': 5000, 'time_ms': 88.96173246515295}, {
-    'desired_samples': 10000, 'time_ms': 166.0814554663375}, {'desired_samples': 50000, 'time_ms': 832.8834265936166}, {'desired_samples': 100000, 'time_ms': 892.8499184704075}]}
+taichi_sample_results = {'taichi_cpu': [{'desired_samples': 1000, 'time_ms': 16.892206738702953}, {'desired_samples': 5000, 'time_ms': 83.55948373985787}, {
+    'desired_samples': 10000, 'time_ms': 167.95633606767905}, {'desired_samples': 50000, 'time_ms': 841.8607126067703}, {'desired_samples': 100000, 'time_ms': 905.9399942712238}]}
 
-julia_sample_results = {'julia_cpu': [{'desired_samples': 1000, 'time_ms': 4.405545396730304}, {'desired_samples': 5000, 'time_ms': 21.63689660374075}, {
-    'desired_samples': 10000, 'time_ms': 44.12896779831499}, {'desired_samples': 50000, 'time_ms': 230.2900455892086}, {'desired_samples': 100000, 'time_ms': 244.81945021543652}]}
+julia_sample_results = {'julia_cpu': [{'desired_samples': 1000, 'time_ms': 4.350052401423454}, {'desired_samples': 5000, 'time_ms': 21.523955394513905}, {
+    'desired_samples': 10000, 'time_ms': 46.494469000026584}, {'desired_samples': 50000, 'time_ms': 228.63133139908314}, {'desired_samples': 100000, 'time_ms': 246.3819775963202}]}
 
 
 def run_benchmarks():
@@ -83,3 +83,6 @@ if __name__ == '__main__':
                  taichi_sample_results)
     else:
         julia_sample_results, numba_sample_results, taichi_sample_results = run_benchmarks()
+        print(julia_sample_results)
+        print(numba_sample_results)
+        print(taichi_sample_results)
